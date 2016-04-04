9c982216de7df85d1b6c7632dfde25c578d004f7
Darin Sikanic <darin.sikanic@gmail.com>
2016-04-04 19:43:14 +1000

    Adding data to database

--- www/www/controllers/api.py
+++ www/www/controllers/api.py
@@ -1,7 +1,9 @@
 from flask import request
 from flask_restful import reqparse, abort, Resource, Api
+from www.extensions import mongo
+# from srmanager.client import Client
 
-api = Api(prefix='/api/v1')
+api = Api(prefix='/api/v1')  
 
 class Service(Resource):
   def get(self, id):
@@ -16,8 +18,29 @@ class Services(Resource):
     return {'hello': 'world'}
 
   def post(self):
-    request.form['data']
-    return {'hello': 'world'}, 201
+    parser = reqparse.RequestParser()
+    parser.add_argument('ingress-switch')
+    parser.add_argument('egress-switch')
+    parser.add_argument('ingress-port')
+    parser.add_argument('egress-port')
+
+    args = parser.parse_args()
+
+    service = {
+      'ingress_switch': args['ingress-switch'],
+      'ingress_port': args['ingress-port'],
+      'egress_switch': args['egress-switch'],
+      'egress_port': args['egress-port']
+    }
+
+    services = mongo.db.services
+    db_id = services.insert_one(service).inserted_id
+
+    srm = Client(config={'ctrlIp': '202.9.5.219', 'ctrlPort': 8181, 'ctrlUser': 'admin', 'ctrlPassword': 'admin'})
+    # srm.add_service(service=service)
+
+    service['_id'] = str(service['_id'])
+    return service, 201
 
 api.add_resource(Services, '/services')
 api.add_resource(Service, '/services/<id>')
