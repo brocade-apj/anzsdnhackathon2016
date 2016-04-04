5ac060bf295e488afc6ede0323fe3d3e26a4b553
Craig Stevens <craig@brocade.com>
2016-04-04 18:56:44 +1000

    use directed graphs

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ srm = srmanager.sr.SR()
 
 top = srm.get_topology()
 
+#for n in top:
+#    for l in top[n]:
+#        print "{} node, {} link, {} src-tp".format(n,l,top[n][l]['source-tp'])
+
 srm.del_all_flows(top)
 srm.add_sr_flows(top)
