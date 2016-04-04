19c599cb26ebc5278aa9f5d9abaa5f81871d8705
Darin Sikanic <darin.sikanic@gmail.com>
2016-04-04 14:15:30 +1000

    Adding database support

--- www/manage.py
+++ www/manage.py
@@ -5,6 +5,7 @@ import os
 from flask.ext.script import Manager, Server
 from flask.ext.script.commands import ShowUrls, Clean
 from www import create_app
+from www.extensions import mongo
 
 # default to dev config because no one should use this in
 # production anyway
@@ -19,11 +20,19 @@ manager.add_command("clean", Clean())
 
 @manager.shell
 def make_shell_context():
-    """ Creates a python REPL with several default imports
-        in the context of the app
-    """
+  """ Creates a python REPL with several default imports
+      in the context of the app
+  """
 
-    return dict(app=app)
+  return dict(app=app, mongo=mongo)
+
+@manager.command
+def create_db():
+  if env == 'dev':
+    tests_collection = mongo.db.tests
+    test = [{'_id': 1, 'name':'darin'}, {'_id': 2, 'name':'jon'}]
+    obj_ids = tests_collection.insert_many(test).inserted_ids
+    return obj_ids
 
 if __name__ == "__main__":
-    manager.run()
+  manager.run()
--- www/www/extensions.py
+++ www/www/extensions.py
@@ -1,8 +1,10 @@
 from flask.ext.cache import Cache
 from flask_assets import Environment
-
+from flask.ext.pymongo import PyMongo
 # Setup flask cache
 cache = Cache()
 
 # init flask assets
 assets_env = Environment()
+
+mongo = PyMongo()
